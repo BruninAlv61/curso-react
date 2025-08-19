e004da1ffff724d59b981ed7a6d68f299751b94f
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-08-19 14:55:04 -0300

    utilizando useReducer para reducir para separar la logica de actualizar el estado del provider, creando su hook y manejando buenas practicas de providers

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -3,9 +3,25 @@ import { ClearCartIcon, CartIcon } from './Icons.jsx'
 import { useCart } from '../hooks/useCart.js'
 import './Cart.css'
 
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title} />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
+
 export function Cart() {
   const cartCheckboxId = useId()
-  const { clearCart } = useCart()
+  const { cart, clearCart, addToCart } = useCart()
 
   return (
     <>
@@ -16,20 +32,13 @@ export function Cart() {
 
       <aside className="cart">
         <ul>
-          <li>
-            <img
-              src="https://images.unsplash.com/photo-1592899677977-9c10ca588bbd?w=400&q=80"
-              alt="iPhone"
+          {cart.map((product) => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
             />
-            <div>
-              <strong>iPhone</strong> - $1400
-            </div>
-
-            <footer>
-              <small>Qty: 1</small>
-              <button>+</button>
-            </footer>
-          </li>
+          ))}
         </ul>
 
         <button onClick={clearCart}>
