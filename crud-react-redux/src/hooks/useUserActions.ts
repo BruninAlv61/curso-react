2922df6162ff86e2cda19ff7ad28c7bb1825c7d4
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-09-29 16:30:48 -0300

    create user

--- crud-react-redux/src/hooks/useUserActions.ts
+++ crud-react-redux/src/hooks/useUserActions.ts
@@ -1,12 +1,16 @@
-import { deleteUserById, type UserId } from '../store/users/slice.ts'
+import { addNewUser, deleteUserById, type UserId } from '../store/users/slice.ts'
 import { useAppDispatch } from './store.ts'
 
 export const useUserActions = () => {
   const dispatch = useAppDispatch()
+  
+  const addUser = ({ name, email, github}) => {
+    dispatch(addNewUser({ name, email, github }))
+  }
 
   const removeUser = (id: UserId) => {
     dispatch(deleteUserById(id))
   }
 
-  return { removeUser }
+  return { addUser, removeUser }
 }
