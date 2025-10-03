016996a430ad916a846c4d6796914a7345c9f556
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-10-03 15:08:29 -0300

    reset original state with useRef and sortedUsers/filteredUsers with useMemo

--- prueba-tecnica-empresa-55k/src/App.tsx
+++ prueba-tecnica-empresa-55k/src/App.tsx
@@ -1,19 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 import './App.css'
 import type { User } from './types.d'
 import { UsersList } from './components/UsersList'
 
 function App() {
   const [users, setUsers] = useState<User[]>([])
+  const originalUsers = useRef<User[]>([]) // así si
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
 
   const toggleColors = () => {
     setShowColors(!showColors)
   }
 
   const toggleSortByCountry = () => {
-    setSortByCountry(prevState => !prevState)
+    setSortByCountry((prevState) => !prevState)
   }
 
   const handleDelete = (email: string) => {
@@ -26,13 +32,30 @@ function App() {
       .then((res) => res.json())
       .then((res) => {
         setUsers(res.results)
+        originalUsers.current = res.results //se modifica el current
       })
       .catch((err) => console.error(err))
   }, [])
 
-  const sortedUsers = sortByCountry ? users.toSorted((a, b) => {
-    return a.location.country.localeCompare(b.location.country)
-  }) : users
+  const filteredUsers = useMemo(() => {
+    console.log('Se están filtrando los usuarios con useMemo')
+    return filterCountry
+    ? users.filter((user) => {
+        return user.location.country
+          .toLocaleLowerCase()
+          .includes(filterCountry.toLowerCase())
+      })
+    : users}, [users, filterCountry])
+
+    const sortedUsers = useMemo(()=> {
+    console.log('Se están ordenando los usuarios con useMemo')
+    return sortByCountry
+      ? filteredUsers.toSorted(
+        (a, b) =>  a.location.country.localeCompare(b.location.country)
+      )
+      : filteredUsers
+    }, [filteredUsers, sortByCountry]) // <-- estas dependencias son los valores que
+                                       // queremos que al cambiar vuelva a ejecutar el useMemo
 
   return (
     <div className="App">
@@ -42,9 +65,21 @@ function App() {
         <button onClick={toggleSortByCountry}>
           {sortByCountry ? 'No ordenar por país' : 'Ordernar por país'}
         </button>
+        <button onClick={handleReset}>Resetear estado</button>
+        <input
+          type="text"
+          placeholder="Filtra por país"
+          onChange={(e) => {
+            setFilterCountry(e.target.value)
+          }}
+        />
       </header>
       <main>
-        <UsersList deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
+        <UsersList
+          deleteUser={handleDelete}
+          showColors={showColors}
+          users={sortedUsers}
+        />
       </main>
     </div>
   )
