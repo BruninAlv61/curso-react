68bdd92012752ad15dfd8c4eb34c1c6530128bd8
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-10-06 15:13:05 -0300

    show more users with pagination

--- prueba-tecnica-empresa-55k/src/App.tsx
+++ prueba-tecnica-empresa-55k/src/App.tsx
@@ -3,15 +3,25 @@ import './App.css'
 import { SortBy, type User } from './types.d'
 import { UsersList } from './components/UsersList'
 
+const fetchUsers = async (page: number) => {
+  return await fetch(`https://randomuser.me/api/?results=10&seed=brunodev&page=${page}`)
+    .then(res => {
+      if(!res.ok) throw new Error('Error en la petición')
+        return res.json()
+    })
+    .then(res => res.results)
+}
+
 function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
   const [filterCountry, setFilterCountry] = useState<string | null>(null)
-  
+
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-  
+  const [currentPage, setCurrentPage] = useState(1)
+
   const originalUsers = useRef<User[]>([])
 
   const handleReset = () => {
@@ -23,7 +33,8 @@ function App() {
   }
 
   const toggleSortByCountry = () => {
-    const newSortingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
+    const newSortingValue =
+      sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
     setSorting(newSortingValue)
   }
 
@@ -39,52 +50,51 @@ function App() {
   useEffect(() => {
     setLoading(true)
     setError(null)
-    fetch('https://randomuser.me/api/?results=10')
-    .then((res) => {
-      if (!res.ok) throw new Error('Error en la petición')
-      return res.json()
-    })
-    .then((res) => {
-        setUsers(res.results)
-        originalUsers.current = res.results
+    fetchUsers(currentPage)
+      .then(users => {
+        setUsers(prevUsers => {
+          const newUsers = prevUsers.concat(users) 
+          originalUsers.current = newUsers
+          return newUsers
+        })
       })
       .catch((err) => {
         setError(err)
         console.error(err)
       })
-      .finally(()=> {
+      .finally(() => {
         setLoading(false)
       })
-  }, [])
+  }, [currentPage])
 
   const filteredUsers = useMemo(() => {
     console.log('Se están filtrando los usuarios con useMemo')
     return filterCountry
-    ? users.filter((user) => {
-        return user.location.country
-          .toLocaleLowerCase()
-          .includes(filterCountry.toLowerCase())
-      })
-    : users}, [users, filterCountry])
+      ? users.filter((user) => {
+          return user.location.country
+            .toLocaleLowerCase()
+            .includes(filterCountry.toLowerCase())
+        })
+      : users
+  }, [users, filterCountry])
 
-    const sortedUsers = useMemo(()=> {
+  const sortedUsers = useMemo(() => {
     console.log('Se están ordenando los usuarios con useMemo')
-    
+
     if (sorting === SortBy.NONE) return filteredUsers
-    
+
     const compareProperties: Record<string, (user: User) => any> = {
-      [SortBy.COUNTRY]: user => user.location.country,
-      [SortBy.NAME] : user => user.name.first,
-      [SortBy.LAST] : user => user.name.last
+      [SortBy.COUNTRY]: (user) => user.location.country,
+      [SortBy.NAME]: (user) => user.name.first,
+      [SortBy.LAST]: (user) => user.name.last
     }
 
     return filteredUsers.toSorted((a, b) => {
       const extractPropierty = compareProperties[sorting]
       return extractPropierty(a).localeCompare(extractPropierty(b))
     })
-
-    }, [filteredUsers, sorting]) // <-- estas dependencias son los valores que
-                                       // queremos que al cambiar vuelva a ejecutar el useMemo
+  }, [filteredUsers, sorting]) // <-- estas dependencias son los valores que
+  // queremos que al cambiar vuelva a ejecutar el useMemo
 
   return (
     <div className="App">
@@ -92,7 +102,9 @@ function App() {
       <header>
         <button onClick={toggleColors}>Colorear filas</button>
         <button onClick={toggleSortByCountry}>
-          {sorting === SortBy.COUNTRY ? 'No ordenar por país' : 'Ordernar por país'}
+          {sorting === SortBy.COUNTRY
+            ? 'No ordenar por país'
+            : 'Ordernar por país'}
         </button>
         <button onClick={handleReset}>Resetear estado</button>
         <input
@@ -104,17 +116,23 @@ function App() {
         />
       </header>
       <main>
+        {users.length > 0 && (
+          <UsersList
+            changeSorting={handleChangeSort}
+            deleteUser={handleDelete}
+            showColors={showColors}
+            users={sortedUsers}
+          />
+        )}
         {loading && <p>Cargando...</p>}
-        {!loading && error && <p>Ha habido un error</p>}
-        {!loading && !error && users.length === 0 && <p>No hay usuarios</p>}
-        {!loading && !error && users.length > 0 && 
-        <UsersList
-          changeSorting={handleChangeSort}
-          deleteUser={handleDelete}
-          showColors={showColors}
-          users={sortedUsers}
-        />}
-        
+        {error && <p>Ha habido un error</p>}
+        {!error && users.length === 0 && <p>No hay usuarios</p>}
+
+        {!loading && !error && users.length > 0 && (
+          <button onClick={() => setCurrentPage(currentPage + 1)}>
+            Cargar más resultados
+          </button>
+        )}
       </main>
     </div>
   )
